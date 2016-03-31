4a12b2d26ad8ad821212cd0c579ce3703f286851
braahmaNa <braahmaNa@gmail.com>
2016-03-31 14:53:05 +0900

    一時コミット

--- js/Battle.js
+++ js/Battle.js
@@ -51,19 +51,32 @@ Class.Extend( Namespace.screen.M_screen, Namespace.screen.M_battle );
 Namespace.screen.V_battle.prototype = {
   foreColor:"#FFFFFF",
   backColor:"#000000",
+  fontSize:18,
+  area_message_x:20,
+  area_message_y250,
+  area_message_width:360,
+  area_message_height:130,
   _initialize: function (){
     with (this.context){
       strokeStyle=this.foreColor;
       lineWidth=5;
       fillStyle=this.foreColor;
       textBaseline="top";
-      font="18px monospace";
-      
-      strokeRect(20,250,360,130);
-      fillText("債権者が現れた！",40,270);
+      font=this.fontSize+"px monospace";
+      strokeRect(this.area_message_x,this.area_message_y,this.area_message_width,this.area_message_height );
+      this.draw_message("債権者が現れた！");
     }
     this.draw_char();
   },
+  draw_message:function(msg){
+    // todo:write crlf
+      this.context.fillStyle=this.foreColor;
+      this.context.fillText(msg,40,270);
+  },
+  clear_message:function(){
+      this.context.fillStyle=this.backColor;
+      this.context.fillRectangle(this.area_message_x,this.area_message_y,this.area_message_width,this.area_message_height );
+  }
   _draw_all: function (){
   },
   draw_char: function (){
